752ee0654959bc15ed29dbe9e9e8fcfd123526c0
joelfern <joelfern@cisco.com>
2017-03-16 09:51:55 +0800

    joel rev

--- step 2/Wrapper_API.py
+++ step 2/Wrapper_API.py
@@ -77,3 +77,57 @@ class Wrapper_API(object):
         apiRequest = Wrapper_API()
         apiResponse = apiRequest.send_api_request(snmpConfigURL)
         return apiResponse
+
+    def createVPN(self):
+        data = '''
+            {
+              "l3vpn:l3vpn": {
+                "name": "%(vpnName)s",
+                "route-distinguisher": %(routeDistinguisher)s,
+                "endpoint": [
+                  {
+                    "id": "%(sourceID)s",
+                    "ce-device": "%(sourceCeDevice)s",
+                    "ce-interface": "%(sourceCeInterface)s",
+                    "ip-network": "%(sourceNetwork)s",
+                    "bandwidth": %(sourceBandwidth)s,
+                    "as-number": %(sourceAsNumber)s
+                  },
+                  {
+                    "id": "%(destinationID)s",
+                    "ce-device": "%(destinationCeDevice)s",
+                    "ce-interface": "%(destinationCeInterface)s",
+                    "ip-network": "%(destinationNetwork)s",
+                    "bandwidth": %(destinationBandwidth)s,
+                    "as-number": %(destinationAsNumber)s
+                  }
+                ]
+              }
+            }
+        ''' % {'vpnName': input("VPN Name: "),
+               'routeDistinguisher': int(input("Route Distinguiser: ")),
+               'sourceID': input("ID: "),
+               'sourceCeDevice': input("CE Device: "),
+               'sourceCeInterface': input("CE Interface: "),
+               'sourceNetwork': input("Network Address: "),
+               'sourceBandwidth': int(input("Bandwidth: ")),
+               'sourceAsNumber': int(input("AS Number: ")),
+               'destinationID': input("ID: "),
+               'destinationCeDevice': input("CE Device: "),
+               'destinationCeInterface': input("CE Interface: "),
+               'destinationNetwork': input("Network Address: "),
+               'destinationBandwidth': int(input("Bandwidth: ")),
+               'destinationAsNumber': int(input("AS Number: "))
+               }
+
+        data_formatted = json.loads(data, object_pairs_hook=OrderedDict)
+
+        with open('createVPN.json', 'w') as outfile:
+            json.dump(data_formatted, outfile, indent=2)
+
+        payload = open('createVPN.json', 'rb').read()
+        response = requests.request("POST", url, data=payload, headers=headers, auth=(user, password))
+
+        print()
+        print(response.text)
+        print("VPN Created")
