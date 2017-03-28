470148d8dacdb9392e850db44e7dcdc54f3a7dec
joelfern <joelfern@cisco.com>
2017-03-28 10:01:17 +0800

    tes

--- step 3/FlaskApp/app.py
+++ step 3/FlaskApp/app.py
@@ -5,17 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
-
-
-@app.route("/")
-def dark():
     return render_template('index-dark.html')
 
 
-# @app.route("/L3VpnDeploy/")
-# def L3VpnDeploy():
-#    return render_template('Backup/L3VpnDeploy.html')
+@app.route("/")
+def L3VpnDeploy():
+    return render_template('L3VpnDeploy.html')
 
 if __name__ == "__main__":
     app.run()
