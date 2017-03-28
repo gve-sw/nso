03c2bd083a644c55878ce9f134972d89f490bfad
Ajay <ajdoshi@cisco.com>
2017-03-28 23:00:23 +0800

    Implemented OrderedDict to resolve issue where NSO API would not accept JSON file which was out of order

--- step 2/Wrapper_API.py
+++ step 2/Wrapper_API.py
@@ -26,6 +26,8 @@
 #           For Cisco partners, open a case at www.cisco.com/go/ph
 
 import requests
+import json
+from collections import OrderedDict
 
 host = '198.18.134.28:8080'
 username = 'admin'
