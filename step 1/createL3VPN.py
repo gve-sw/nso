2095dd301d083cd08eaf7653eeea0431d20ac0c9
joelfern <joelfern@cisco.com>
2017-03-28 11:14:44 +0800

    u

---
+++ step 1/createL3VPN.py
@@ -0,0 +1,85 @@
+import json
+import requests
+from collections import OrderedDict
+
+url = "http://198.18.134.28:8080/api/running/vpn"
+user = 'admin'
+password = 'admin'
+headers = {
+        'content-type': "application/vnd.yang.data+json",
+        }
+
+def main():
+    """
+    Main method to start this L3VPN deployment tool.
+    """
+
+    print("######################################################")
+    print("####                                              ####")
+    print("####  NSO L3VPN Deployment Tool                   ####")
+    print("####                                              ####")
+    print("####  Developer: Joel Fernandez, Ajay Doshi       ####")
+    print("####  Email: joelfern@cisco.com                   ####")
+    print("####                                              ####")
+    print("######################################################")
+    print()
+    print("This tool uses the northbound NSO API to deploy L3VPNs")
+    print()
+
+    data = '''
+        {
+          "l3vpn:l3vpn": {
+            "name": "%(vpnName)s",
+            "route-distinguisher": %(routeDistinguisher)s,
+            "endpoint": [
+              {
+                "id": "%(sourceID)s",
+                "ce-device": "%(sourceCeDevice)s",
+                "ce-interface": "%(sourceCeInterface)s",
+                "ip-network": "%(sourceNetwork)s",
+                "bandwidth": %(sourceBandwidth)s,
+                "as-number": %(sourceAsNumber)s
+              },
+              {
+                "id": "%(destinationID)s",
+                "ce-device": "%(destinationCeDevice)s",
+                "ce-interface": "%(destinationCeInterface)s",
+                "ip-network": "%(destinationNetwork)s",
+                "bandwidth": %(destinationBandwidth)s,
+                "as-number": %(destinationAsNumber)s
+              }
+            ]
+          }
+        }
+    ''' % {'vpnName': input("VPN Name: "),
+           'routeDistinguisher': int(input("Route Distinguiser: ")),
+           'sourceID': input("ID: "),
+           'sourceCeDevice': input("CE Device: "),
+           'sourceCeInterface': input("CE Interface: "),
+           'sourceNetwork': input("Network Address: "),
+           'sourceBandwidth': int(input("Bandwidth: ")),
+           'sourceAsNumber': int(input("AS Number: ")),
+           'destinationID': input("ID: "),
+           'destinationCeDevice': input("CE Device: "),
+           'destinationCeInterface': input("CE Interface: "),
+           'destinationNetwork': input("Network Address: "),
+           'destinationBandwidth': int(input("Bandwidth: ")),
+           'destinationAsNumber': int(input("AS Number: "))
+           }
+
+    data_formatted = json.loads(data, object_pairs_hook=OrderedDict)
+
+    with open('createVPN.json', 'w') as outfile:
+        json.dump(data_formatted, outfile, indent=2)
+
+    payload = open('createVPN.json', 'rb').read()
+    response = requests.request("POST", url, data=payload, headers=headers, auth=(user, password))
+
+    print()
+    print(response.text)
+    print("VPN Created")
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ step 3/FlaskApp/static/assets/js/custom.js
@@ -0,0 +1,83 @@
+﻿
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% Free To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        scroll_fun: function () {
+
+            //CUSTOM SCROLL SCRIPT FUNCTION FOR NAVBAR 
+          $(function () {
+              $('.scrollclass a').bind('click', function (event) { //just pass scrollclass in design and start scrolling
+                  var $anchor = $(this);
+                  $('html, body').stop().animate({
+                      scrollTop: $($anchor.attr('href')).offset().top
+                  }, 1200, 'easeInOutExpo');
+                  event.preventDefault();
+              });
+          });
+            
+
+        },
+        
+        gallery_fun: function () {
+            /*====================================
+                 FOR IMAGE/GALLERY POPUP
+            ======================================*/
+            $("a.preview").prettyPhoto({
+                social_tools: false
+            });
+
+            /*====================================
+            FOR IMAGE/GALLERY FILTER
+            ======================================*/
+
+            // MixItUp plugin
+            // http://mixitup.io
+
+            $('#port-folio').mixitup({
+                targetSelector: '.portfolio-item',
+                filterSelector: '.filter',
+
+
+            });
+        },
+        nicescroll_fun:function()
+        {
+            $("html").niceScroll();
+        },
+
+      
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    };
+   
+   
+    $(document).ready(function () {
+        mainApp.scroll_fun();
+        mainApp.gallery_fun();
+        mainApp.nicescroll_fun();
+        mainApp.custom_fun();
+
+    });
+}(jQuery));
+
+
