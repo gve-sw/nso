1a7b4c6268992d18f459c79e4470bfcb43dcd28f
joelfern <joelfern@cisco.com>
2017-03-29 13:33:27 +0800

    u

--- step 3/FlaskApp/app.py
+++ step 3/FlaskApp/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
@@ -11,6 +10,15 @@ def main():
 @app.route("/L3VpnDeploy")
 def L3VpnDeploy():
     return render_template('L3VpnDeploy.html')
+
+@app.route("/L3VpnDeploy. methods ['POST]")
+def createJSON():
+    newVPN = request.form["JSON"]
+    json dumps
+    send api request
+    json delete
+    print(newVPN)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run()
