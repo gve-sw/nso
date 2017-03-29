0e80132b8d362734fe018599c74e46694f123fd8
joelfern <joelfern@cisco.com>
2017-03-29 21:22:21 +0800

    Working Portal VPN Create

--- step 3/L3VPN_Deploy.py
+++ step 3/L3VPN_Deploy.py
@@ -1,7 +1,8 @@
+import json
+from collections import OrderedDict
 from Wrapper_API import Wrapper_API
 
-
-host = '198.18.134.28:8080'
+host = "http://198.18.134.28:8080/api/running/vpn"
 username = 'admin'
 password = 'admin'
 headers = {
@@ -22,12 +23,63 @@ def main():
     print("####                                              ####")
     print("######################################################")
     print()
-    print("This tool uses the northbound NSO API to deploy L3VPNs")
+    print("This tool uses the nourthbound NSO API to deploy L3VPNs")
     print()
 
-    api = Wrapper_API(host, username, password)
-    #Not working anymore because we changed our create VPN function.
-    api.createVPN()
+    data = '''
+        {
+          "l3vpn:l3vpn": {
+            "name": "%(vpnName)s",
+            "route-distinguisher": %(routeDistinguisher)s,
+            "endpoint": [
+              {
+                "id": "%(sourceID)s",
+                "ce-device": "%(sourceCeDevice)s",
+                "ce-interface": "%(sourceCeInterface)s",
+                "ip-network": "%(sourceNetwork)s",
+                "bandwidth": %(sourceBandwidth)s,
+                "as-number": %(sourceAsNumber)s
+              },
+              {
+                "id": "%(destinationID)s",
+                "ce-device": "%(destinationCeDevice)s",
+                "ce-interface": "%(destinationCeInterface)s",
+                "ip-network": "%(destinationNetwork)s",
+                "bandwidth": %(destinationBandwidth)s,
+                "as-number": %(destinationAsNumber)s
+              }
+            ]
+          }
+        }
+    ''' % {'vpnName': input("VPN Name: "),
+           'routeDistinguisher': int(input("Route Distinguiser: ")),
+           'sourceID': input("ID: "),
+           'sourceCeDevice': input("CE Device: "),
+           'sourceCeInterface': input("CE Interface: "),
+           'sourceNetwork': input("Network Address: "),
+           'sourceBandwidth': int(input("Bandwidth: ")),
+           'sourceAsNumber': int(input("AS Number: ")),
+           'destinationID': input("ID: "),
+           'destinationCeDevice': input("CE Device: "),
+           'destinationCeInterface': input("CE Interface: "),
+           'destinationNetwork': input("Network Address: "),
+           'destinationBandwidth': int(input("Bandwidth: ")),
+           'destinationAsNumber': int(input("AS Number: "))
+           }
+
+    data_formatted = json.loads(data, object_pairs_hook=OrderedDict)
+
+    with open('createVPN.json', 'w') as outfile:
+        json.dump(data_formatted, outfile, indent=2)
+
+    payload = open('createVPN.json', 'rb').read()
+
+    apiRequest = Wrapper_API(host, username, password)
+    apiRequest.createVPN(data_formatted)
+
+    print()
+    print(apiRequest.text)
+    print("VPN Created")
 
 
 if __name__ == "__main__":
